d2a262e6b53859ca8a4bd5e33a4ae4555a58fdf8
HAMED POUYA <hpouyaofficial@gmail.com>
2026-09-06 08:40:40 +0000

    edit

--- automation/Automation_Testing/tests/test_home.py
+++ automation/Automation_Testing/tests/test_home.py
@@ -1,12 +1,12 @@
 from playwright.sync_api import Page, expect
 
 
-def test_home_page(Page):
-    Page.goto("http://127.0.0.1:8000/")
-    Page.get_by_text("Login").click()
-    Page.locator('[name="username"]').fill("hamedx")
-    Page.locator('[name="password"]').fill("hH19921992@")
-    Page.get_by_role('button', name="Login").click()
+def test_home_page(page):
+    page.goto("http://127.0.0.1:8000/")
+    page.get_by_text("Login").click()
+    page.locator('[name="username"]').fill("hamedx")
+    page.locator('[name="password"]').fill("gvg55485fef")
+    page.get_by_role('button', name="Login").click()
     #page.pause()
 
-    expect(Page).to_have_title("GODSOFSTRATEGY")
+    expect(page).to_have_title("GODSOFSTRATEGY")
